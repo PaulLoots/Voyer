01bd80f2f56b082bad9572ad909879df264f5549
Paul Loots <170083@virtualwindow.co.za>
2020-03-18 17:45:04 +0200

    feat: Create Project + Setup Realitykit Environment

---
+++ Voyer/Voyer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Voyer
+//
+//  Created by Paul Loots on 2020/03/14.
+//  Copyright © 2020 Paul Loots. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
